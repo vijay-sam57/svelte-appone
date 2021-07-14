cfb1fa953ef9b2241b226c95db340ad52b2ce24f
Vijay Sam <vijay203.vk@gmail.com>
2021-07-14 12:36:47 +0530

    Bindings learned

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,19 @@
 <script>
-	export let name;
+  const name = "Sam allen";
+  const ht = "<h2><em>Sam<em/>Allen<h2/>";
+  const alt = "This is an alt text";
+  const disabled = true;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h2>Hello Mr.{name}</h2>
+  <h1>This is from script :{@html ht}</h1>
+  <img {alt} />
+  <button {disabled}>this a button</button>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  h1 {
+    color: #f25fff;
+  }
 </style>
