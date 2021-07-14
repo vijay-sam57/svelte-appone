4d793218c38c4d76523a3016c6202f36101077c8
Vijay Sam <vijay203.vk@gmail.com>
2021-07-14 23:32:13 +0530

    If Else Statement learned

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,28 @@
 <script>
-  const name = "Sam allen";
-  const ht = "<h2><em>Sam<em/>Allen<h2/>";
-  const alt = "This is an alt text";
-  const disabled = true;
-  const theme = "blk";
-  const size = "med";
-  const finished = true;
+  // const name = "Sam allen";
+  // const ht = "<h2><em>Sam<em/>Allen<h2/>";
+  // const alt = "This is an alt text";
+  // const disabled = true;
+  // const theme = "blk";
+  // const size = "med";
+  // const finished = true;
+  let age = 17;
 </script>
 
 <main>
-  <h2>Hello Mr.{name}</h2>
+  <!-- <h2>Hello Mr.{name}</h2>
   <h1>This is from script :{@html ht}</h1>
   <img {alt} />
   <button {disabled}>this a button</button>
   <h2 class={(finished ? "finished" : "", theme)}>This is for class binding</h2>
-  <h2 class={size}>This is for class binding</h2>
+  <h2 class={size}>This is for class binding</h2> -->
+  {#if age >= 18}
+    <h3>You are eligible to vote</h3>
+  {:else if age === 17}
+    <h3>Dont worry you can vote in a year</h3>
+  {:else}
+    <h3>You are not elgible to vote</h3>
+  {/if}
 </main>
 
 <style>
